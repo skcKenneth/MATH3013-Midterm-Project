e7e83816373ad4d5f81857a8928a9eb8b655cab8
Kenneth, Sok-kin Cheng <46993305+sokkin-cheng@users.noreply.github.com>
2022-12-02 17:34:53 +0800

    Add files via upload
    Make some changes

---
+++ HC011292-Runge_Kutta_Method/RK4.h
@@ -0,0 +1,71 @@
+#ifndef RK4_H // include guard
+#define RK4_H
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+class RungeKutta {
+protected:
+	double sigma = 10.;
+	double R = 28.;
+	double b = 10. / 3.;
+protected:
+	double t1, t2, t3;
+	double* f1, * f2, * f3, * f4;
+	double* x, * x1, * x2, * x3;
+public:
+	double* RK4(double t0, double x0[], double h);
+	double* ls(double x[]);
+};
+
+//Lorenz System
+double* RungeKutta::ls(double x[]) {
+	double* dxdt = new double[3];
+
+	dxdt[0] = sigma * (x[1] - x[0]);
+	dxdt[1] = R * x[0] - x[1] - x[0] * x[2];
+	dxdt[2] = x[0] * x[1] - b * x[2];
+	return dxdt;
+}
+//Runge-Kutta-Methods
+double* RungeKutta::RK4(double t0, double x0[], double h) {
+
+	f1 = ls(x0);
+	t1 = t0 + h / 2;
+	x1 = new double[3];
+	for (int i = 0; i < 3; i++) {
+		x1[i] = x0[i] + h * f1[i] / 2.0;
+	}
+
+	f2 = ls(x1);
+	t2 = t0 + h / 2;
+	x2 = new double[3];
+	for (int i = 0; i < 3; i++) {
+		x2[i] = x0[i] + h * f2[i] / 2.0;
+	}
+
+	f3 = ls(x2);
+	t3 = t0 + h;
+	x3 = new double[3];
+	for (int i = 0; i < 3; i++) {
+		x3[i] = x0[i] + h * f3[i];
+	}
+
+	f4 = ls(x3);
+	x = new double[3];
+	for (int i = 0; i < 3; i++) {
+		x[i] = x0[i] + h * (f1[i] + 2.0 * f2[i] + 2.0 * f3[i] + f4[i]) / 6.0;
+	}
+	delete[] f1;
+	delete[] f2;
+	delete[] f3;
+	delete[] f4;
+	delete[] x1;
+	delete[] x2;
+	delete[] x3;
+	return x;
+}
+#endif /* RK4_H */
--- HC011292-Runge_Kutta_Method/Source.cpp
+++ HC011292-Runge_Kutta_Method/Source.cpp
@@ -1,19 +1,15 @@
-#include "RG4.h"
+#include "RK4.h"
 using namespace std;
 int main() {
-	double t0 = 0.;
+	double ti = 0.;
 	double tf = 40.;
 	int n = 9e4; //Number of points in RK4, it can be changed
 	double delta_t;
-	double* values = new double[n];
-	RungeKutta RG; // Using the function from the class
-	double* val;
+	RungeKutta Runge; // Using the function from the class
+	double* values;
 	double x_int[3] = { 1., 1., 1. }; // Initial points 
-	vector<double> v1(n);
-	vector<double> v2(n);
-	delta_t = (tf - t0) / (double)(n);
-	int j = 0;
-	val = RG.RK4(t0, x_int, delta_t);
+	delta_t = (tf - ti) / (double)(n);
+	values = Runge.RK4(ti, x_int, delta_t);
 	//Open dataes to write output
 	//.dat is for plotting by Gnuplot
 	//I also has made a file that is .csv for plotting by matplotlib but I do not upload it 
@@ -21,12 +17,12 @@ int main() {
 	data << x_int[0] << "     " << x_int[1] << "     " << x_int[2] << "\n";//For writing the initial 3 points in data
 
 	for (int i = 1; i < n; i++) {
-		val =  RG.RK4(t0, val, delta_t);
-		data << val[0] << "     " << val[1] << "     " << val[2] << "\n";
+		values =  Runge.RK4(ti, values, delta_t);
+		data << values[0] << "     " << values[1] << "     " << values[2] << "\n";
 
 	}
 	//Closing data
 	data.close();
-	delete[] val;
+	delete[] values;
 	return 0;
 }
